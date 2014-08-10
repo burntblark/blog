d8349d6850ca9b8de6ea0e3087f1cdde79088584
Tunde Familusi <f.babatunde@rom-flex.com>
2014-08-10 18:31:50 +0100

    Added stutus field to users; created a helper for statuses that use boolean; Re-organised the layout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140810151438) do
+ActiveRecord::Schema.define(version: 20140810164545) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20140810151438) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password",   limit: 25
+    t.boolean  "status"
   end
 
 end
